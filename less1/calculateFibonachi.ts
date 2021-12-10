70799a61d5fc668daa4bdfbe44c071958284da28
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-12-10 17:19:41 +0200

    Update calculateFibonachi.ts

--- less1/calculateFibonachi.ts
+++ less1/calculateFibonachi.ts
@@ -1,22 +1,27 @@
-let calculateFibonachi = function(): Function {
+let calculateFibonachi = function():Function {
   let cache: number[] = [];
   return function calculateFibonachiRecurcion(numeric: number, result?: number[], count?: number): number {
-    if (cache[numeric]) {
-      return cache[numeric];
-    }
     if (typeof arguments[0] === 'undefined') {
       throw new Error();
     }
-    result = result || [0, 1];
-    count = count || 0;
-    let start = result[result.length - 2];
-    let stop = result[result.length - 1];
-    let sum = start + stop;
-    if (count < numeric - 1) {
-      result.push(sum);
-      return calculateFibonachiRecurcion(numeric, result, ++count);
+    if (cache[numeric]) {
+      return cache[numeric];
     }
-    cache = result;
-    return cache[numeric];
+    if(cache.length){
+      cache[cache.length] = cache[cache.length - 2] + cache[cache.length - 1];
+      return calculateFibonachiRecurcion(numeric, result, count);
+    } else {
+      result = result || [0, 1];
+      count = count || 0;
+      let start = result[result.length - 2];
+      let stop = result[result.length - 1];
+      let sum = start + stop;
+      if (count < numeric - 1) {
+        result.push(sum);
+        return calculateFibonachiRecurcion(numeric, result, ++count);
+      }
+      cache = result;
+      return cache[numeric];
+    }
   }
 }
