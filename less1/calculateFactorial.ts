25bef3219261b828a89c118c772cb697cf636496
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-12-10 16:34:58 +0200

    Update calculateFactorial.ts

--- less1/calculateFactorial.ts
+++ less1/calculateFactorial.ts
@@ -3,14 +3,23 @@ let calculateFactorial = function(): Function {
   return function calculateFactorialRecursion(num: number, count?: number, result?: number[]): number {
     if (cache[num]) {
       return cache[num];
+  }
+    if(cache.length){
+      if(cache[num]){
+        return cache[num];
+      } else {
+        cache[cache.length] = cache[cache.length -1] * cache.length;
+        return calculateFactorialRecursion(num, count, result);
+      }
+    } else {
+      result = result || [1, 1];
+      count = count || 2;
+      if (typeof result[num] === 'undefined') {
+          result.push((result[result.length - 1]) * count);
+          return calculateFactorialRecursion(num, ++count, result);
+      }
+      cache = result;
+      return cache[num];
     }
-    result = result || [1, 1];
-    count = count || 2;
-    if (typeof result[num] === 'undefined') {
-      result.push((result[result.length - 1]) * count);
-      return calculateFactorialRecursion(num, ++count, result);
-    }
-    cache = result;
-    return cache[num];
   }
 }
