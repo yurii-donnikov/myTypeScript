f3cc16120d80bd808625ce92a97d215fcd266547
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-12-13 13:27:44 +0200

    Update sort_solution.ts

--- less3/sort_solution.ts
+++ less3/sort_solution.ts
@@ -1,21 +1,25 @@
+interface ISort {
+  giveIndex: () => number;
+}
+
 interface Array<T> {
   sortSelection(callback: Function): void;
   sortBubble(callback: Function): void;
 }
 
 Array.prototype.sortSelection = function(callback: Function): void {
-  let array: number[] = this;
+  let array: ISort[] = this;
   for (let i: number = 0; i < array.length; i++) {
-    let resultElement: number | string = array[i];
+    let resultElement: ISort = array[i];
     let index: number = 0;
     for (let j: number = i + 1; j < array.length; j++) {
-      if (callback(resultElement, array[j])) {
+      if (callback(resultElement.giveIndex(), array[j].giveIndex())) {
         resultElement = array[j];
         index = j;
       }
     }
     if (index !== 0) {
-      let temp: number | string = array[i];
+      let temp: ISort = array[i];
       array[i] = resultElement;
       array[index] = temp;
     }
@@ -23,11 +27,11 @@ Array.prototype.sortSelection = function(callback: Function): void {
 }
 
 Array.prototype.sortBubble = function(callback: Function): void {
-  let array: number[] = this;
+  let array: ISort[] = this;
   for (let i: number = array.length - 1; i > 0; i--) {
     for (let j: number = 0; j < i; j++) {
-      if (callback(array[j], array[j + 1])) {
-        let copyElement: number | string = array[j];
+      if (callback(array[j].giveIndex(), array[j + 1].giveIndex())) {
+        let copyElement: ISort = array[j];
         array[j] = array[j + 1];
         array[j + 1] = copyElement;
       }
